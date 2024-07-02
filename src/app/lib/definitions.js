91823aa20cfdcfe42bd4ee6d1d1f512e584f80aa
Silas Nevstad <nevstads@gmail.com>
2024-07-02 10:33:34 -0400

    nit

--- src/app/lib/definitions.js
+++ src/app/lib/definitions.js
@@ -2,13 +2,13 @@ import { z } from 'zod'
 
 export const LoginFormSchema = z.object({
     email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-    password: z.string().min(8, { message: 'Be at least 8 characters long' }).trim(),
+    password: z.string().min(8, { message: 'Must be at least 8 characters long' }).trim(),
 })
 
 export const SignupFormSchema = z.object({
     email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
     password: z
         .string()
-        .min(8, { message: 'Be at least 8 characters long' })
+        .min(8, { message: 'Must be at least 8 characters long' })
         .trim(),
 })
