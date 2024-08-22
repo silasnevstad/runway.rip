3642ed460551a9a9f5d5e9017b32ac069fb324a3
Silas Nevstad <nevstads@gmail.com>
2024-08-22 17:36:35 -0400

    Input

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -3,7 +3,15 @@
 import React, { useState } from "react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
-const Input = ({ label, leftIcon: LeftIcon, rightIcon: RightIcon, secure, ...props }) => {
+const Input = ({
+    label,
+    leftIcon: LeftIcon,
+    rightIcon: RightIcon,
+    leftIconOnClick,
+    rightIconOnClick,
+    secure,
+    ...props
+}) => {
     const [showPassword, setShowPassword] = useState(false);
     const [isFocused, setIsFocused] = useState(false);
     const [value, setValue] = useState(props.value || '');
@@ -23,13 +31,21 @@ const Input = ({ label, leftIcon: LeftIcon, rightIcon: RightIcon, secure, ...pro
     return (
         <div className="flex flex-col w-full gap-2">
             <div className="relative flex items-center rounded-lg border border-gray-500 p-1 dark:border-gray-700 focus-within:border-primary-500 focus-within:ring-1 focus-within:ring-primary-500">
-                {LeftIcon && <LeftIcon className="flex-shrink-0 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500 ml-2"/>}
+                {LeftIcon &&
+                    <LeftIcon
+                        className={
+                            `flex-shrink-0 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500 ml-2 
+                            ${leftIconOnClick ? 'cursor-pointer hover:scale-103 hover:text-primary-500' : ''}`
+                        }
+                        onClick={leftIconOnClick}
+                    />
+                }
                 {label && (!value || isFocused) &&
                     <label
                         onClick={() => document.getElementById(props.id).focus()}
                         className={`absolute transition-all duration-200 cursor-text ${
-                            isFocused || props.value 
-                                ? '-top-3.5 left-2 px-2 bg-bg-50 dark:bg-bg-900 text-primary-500 text-opacity-80' 
+                            isFocused || props.value
+                                ? '-top-3.5 left-2 px-2 bg-bg-50 dark:bg-bg-900 text-primary-500 text-opacity-80'
                                 : 'text-lg left-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-gray-600'
                         }`}
                     >
@@ -51,7 +67,15 @@ const Input = ({ label, leftIcon: LeftIcon, rightIcon: RightIcon, secure, ...pro
                         {showPassword ? <EyeSlashIcon className="mr-2 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500"/> : <EyeIcon className="mr-2 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500"/>}
                     </button>
                 )}
-                {RightIcon && <RightIcon className="mr-2 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500"/>}
+                {RightIcon &&
+                    <RightIcon
+                        className={
+                            `mr-2 w-5 h-5 text-gray-600 opacity-75 dark:text-gray-500 
+                            ${rightIconOnClick ? 'cursor-pointer hover:scale-103 hover:text-primary-500' : ''}`
+                        }
+                        onClick={rightIconOnClick}
+                    />
+                }
             </div>
         </div>
     );
