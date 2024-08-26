f5ea80b851a7567f3f370879f642c332cb03f462
Silas Nevstad <nevstads@gmail.com>
2024-08-26 09:48:55 -0400

    policies: empty layout fix

---
+++ src/app/(noauth)/policies/layout.js
@@ -0,0 +1,7 @@
+export default function PolicyLayout({ children }) {
+    return (
+        <section className="flex flex-col items-center w-full min-h-screen">
+            {children}
+        </section>
+    );
+}
