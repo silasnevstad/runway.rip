6932bfb97870bbdca94af30474d2c436e9cfb9bd
Silas Nevstad <nevstads@gmail.com>
2024-09-11 20:50:34 -0400

    tailwind.config: safelist opacities

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 
 const colorTones = [100, 200, 300, 400, 500, 600, 700, 800, 900];
 const colors = ["bg", "primary", "gray", "red", "yellow", "orange", "green", "blue", "purple"];
+const opacities = [0, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100];
 
 module.exports = {
     // darkMode: 'class',
@@ -14,6 +15,9 @@ module.exports = {
         ...colors.map((color) => colorTones.map((tone) => `bg-${color}-${tone}`)).flat(),
         ...colors.map((color) => colorTones.map((tone) => `text-${color}-${tone}`)).flat(),
         ...colors.map((color) => colorTones.map((tone) => `border-${color}-${tone}`)).flat(),
+        ...opacities.map((opacity) => colors.map((color) => colorTones.map((tone) => `bg-${color}-${tone}/${opacity}`)).flat()).flat(),
+        ...opacities.map((opacity) => colors.map((color) => colorTones.map((tone) => `text-${color}-${tone}/${opacity}`)).flat()).flat(),
+        ...opacities.map((opacity) => colors.map((color) => colorTones.map((tone) => `border-${color}-${tone}/${opacity}`)).flat()).flat(),
     ],
     theme: {
         colors: {
