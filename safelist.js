8db3f8de67a5525dbb02056f04a298ee86033543
Silas Nevstad <nevstads@gmail.com>
2025-02-20 12:55:30 -0500

    nit

--- safelist.js
+++ safelist.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const colorTones = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 const colors = ["bg", "primary", "gray", "red", "yellow", "orange", "green", "blue", "purple"];
-const opacities = [0, 5, 10, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100];
+const opacities = [0, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100];
 const gradients = ["from", "to"];
 
 const widths = [0.5, 1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96];
