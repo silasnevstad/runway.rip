517fa93641acc8f4c946bf665b4b70e17d5bff76
Silas Nevstad <nevstads@gmail.com>
2025-03-04 00:30:38 -0500

    [

--- src/libs/supabase/config.js
+++ src/libs/supabase/config.js
@@ -7,15 +7,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
     throw new Error("Supabase URL and anon key must be set in your environment variables.");
 }
 
-// add CORS options
-const corsOptions = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
-const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    global: {
-        headers: corsOptions,
-    },
-});
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export { supabase };
