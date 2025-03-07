d6a8a3b269d2962596a0d07ee2071091781e76a9
Silas Nevstad <nevstads@gmail.com>
2025-03-06 19:19:19 -0500

    p

--- src/app/(noauth)/login/page.js
+++ src/app/(noauth)/login/page.js
@@ -11,7 +11,7 @@ export default function LoginPage() {
     return (
         <div className="flex flex-col items-center w-full -mt-20 gap-2">
             <div className="flex flex-col items-center gap-2">
-                <h1 className="text-3xl font-semibold">Welcome back!</h1>
+                <h1 className="max-[430px]:text-2xl text-3xl font-semibold">Welcome back!</h1>
                 <p className="text-lg text-gray-500">Sign in to your account</p>
             </div>
             <div className="flex flex-col items-center justify-center w-full max-w-md p-4">
@@ -19,7 +19,7 @@ export default function LoginPage() {
                     <AuthForm mode="sign-in"/>
                 </div>
             </div>
-            <div className="absolute top-8 right-8 flex flex-col items-end opacity-90 hover:opacity-100">
+            <div className="sm:absolute top-8 right-8 flex flex-col sm:items-center items-end opacity-90 hover:opacity-100">
                 <p className="opacity-60 text-sm">New around here?</p>
                 <TextLink href="/signup" className="text-primary-500 hover:opacity-100">
                     Create an account
--- src/app/(noauth)/signup/page.js
+++ src/app/(noauth)/signup/page.js
@@ -11,7 +11,7 @@ export default function SignUpPage() {
     return (
         <div className="flex flex-col items-center w-full -mt-20 gap-2">
             <div className="flex flex-col items-center gap-2">
-                <h1 className="text-3xl font-semibold">Create an account</h1>
+                <h1 className="max-[430px]:text-2xl text-3xl font-semibold">Create an account</h1>
                 <p className="text-lg text-gray-500">Let's get you started</p>
             </div>
             <div className="flex flex-col items-center justify-center w-full max-w-md p-4">
@@ -19,7 +19,7 @@ export default function SignUpPage() {
                     <AuthForm mode={"signup"} />
                 </div>
             </div>
-            <div className="absolute top-8 right-8 flex flex-col items-end opacity-90 hover:opacity-100">
+            <div className="sm:absolute top-8 right-8 flex flex-col sm:items-center items-end opacity-90 hover:opacity-100">
                 <p className="opacity-60 text-sm">Already have an account?</p>
                 <TextLink href="/login" className="text-primary-500 hover:opacity-100">
                     Sign in
