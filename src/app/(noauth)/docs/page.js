3148db64960c3957487c769238ed1cf2211e1315
Silas Nevstad <nevstads@gmail.com>
2025-03-09 00:22:49 -0500

    p

--- src/app/(noauth)/docs/page.js
+++ src/app/(noauth)/docs/page.js
@@ -31,36 +31,10 @@ export default function DocsIndexPage() {
                 </div>
                 <div className="flex flex-col gap-4 mb-6 w-full">
                     <h2 className="text-2xl font-semibold">Recommended Tutorials</h2>
-                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
                         <TutorialCard title="Installing" description="Get Runway up and running locally" href="/docs/installation" />
                         <TutorialCard title="Launch in 5 minutes" description="Get started with Runway" href="/docs/tutorial/launch-asap" />
                         <TutorialCard title="Config.js" description="Configure the app to your needs" href="/docs/tutorial/config" />
-                        <TutorialCard title="Config.js" description="Configure the app to your needs" href="/docs/tutorial/config" />
-                        <Badge color="gray" variant="soft">Text</Badge>
-                        <Badge color="gray" variant="solid">Text</Badge>
-                        <Badge color="gray" variant="soft" border>Text</Badge>
-                        <Badge color="gray" variant="solid" border>Text</Badge>
-                        <Badge color="bg" variant="soft">Text</Badge>
-                        <Badge color="bg" variant="solid">Text</Badge>
-                        <Badge color="bg" variant="soft" border>Text</Badge>
-                        <Badge color="bg" variant="solid" border>Text</Badge>
-
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="gray" variant="soft" />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="gray" variant="solid" />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="gray" variant="soft" border />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="gray" variant="solid" border />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="bg" variant="soft" />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="bg" variant="solid" />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="bg" variant="soft" border />
-                        <Switcher selected={"1"} options={[{ value: "1", name: "Option 1" }, { value: "2", name: "Option 2" }]} color="bg" variant="solid" border />
-                        <Input variant="soft" placeholder="Search" />
-                        <Input variant="solid" placeholder="Search" />
-                        <Input variant="soft" placeholder="Search" border />
-                        <Input variant="solid" placeholder="Search" border />
-                        <Input color="bg" variant="soft" placeholder="Search" />
-                        <Input color="bg" variant="solid" placeholder="Search" />
-                        <Input color="bg" variant="soft" placeholder="Search" border />
-                        <Input color="bg" variant="solid" placeholder="Search" border />
                     </div>
                 </div>
             </div>
