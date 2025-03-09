4ddfecf6d4de90b397f3da9dc93abd61b8d48b0e
Silas Nevstad <nevstads@gmail.com>
2025-03-09 00:32:25 -0500

    s

--- safelist.js
+++ safelist.js
@@ -1,3 +1,8 @@
+// TO dynamically use Tailwind CSS (i.e. `text-${color}-500`), that class must be in a safelist.
+// Runway avoids this by using styling.js, however, if you want to create your own components
+// you can use this  script to generate a safelist of all the classes you might need.
+// Also add `@source "safelist.txt";` to your `globals.css` file.
+
 const fs = require("fs");
 
 const colorTones = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
@@ -5,31 +10,6 @@ const colors = ["bg", "primary", "gray", "red", "yellow", "orange", "green", "bl
 const opacities = [0, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 75, 80, 90, 95, 100];
 const gradients = ["from", "to"];
 const borderRadius = ["xs", "sm", "md", "lg", "xl", "2xl", "3xl", "full"];
-
-const widths = [0.5, 1, 2, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96];
-const margins = [0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 72, 80, 96];
-const topPositions = [250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300];
-
-const measurements = [
-    "w-full",
-    "h-full",
-    ...widths.map((width) => `w-${width}`),
-    ...widths.map((width) => `h-${width}`),
-    ...margins.map((width) => `mx-${width}`),
-    ...margins.map((width) => `my-${width}`),
-    ...margins.map((width) => `mt-${width}`),
-    ...margins.map((width) => `mr-${width}`),
-    ...margins.map((width) => `mb-${width}`),
-    ...margins.map((width) => `ml-${width}`),
-    ...margins.map((width) => `px-${width}`),
-    ...margins.map((width) => `py-${width}`),
-    ...margins.map((width) => `pt-${width}`),
-    ...margins.map((width) => `pr-${width}`),
-    ...margins.map((width) => `pb-${width}`),
-    ...margins.map((width) => `pl-${width}`),
-];
-
-const topPositionsClasses = topPositions.map((top) => `top-${top}`);
 
 const safelist = [
     ...colors.map((color) => colorTones.map((tone) => `bg-${color}-${tone}`)).flat(),  // bg-color-tone
@@ -125,10 +105,9 @@ const allSafelist = [
     ...darkSafeList,
     ...hoverSafeList,
     ...darkHoverSafeList,
-    ...measurements,
-    ...topPositionsClasses,
     ...borderRadiusClasses
 ];
 
 fs.writeFileSync("safelist.txt", allSafelist.join("\n"));
+
 console.log("safelist.txt has been generated successfully.");
