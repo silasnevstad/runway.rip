817c6ca28ef528e54c14c3212443d04336e201a4
Silas Nevstad <nevstads@gmail.com>
2025-03-15 00:32:13 -0400

    p

--- src/libs/supabase/db.js
+++ src/libs/supabase/db.js
@@ -4,33 +4,6 @@ import { supabase } from "@/libs/supabase/config";
 import { createClient } from '@/utils/supabase/server';
 import appConfig from '@/config';
 
-// fetchProfile is optional
-export async function fetchProfile() {
-    if (!appConfig.auth.profiles) return null;
-
-    const supabaseClient = createClient();
-    // Always re-validate the user on the server:
-    const {
-        data: { user },
-    } = await supabaseClient.auth.getUser();
-    if (!user) {
-        return null;
-    }
-
-    // If user is logged in, get their profile row
-    const { data, error } = await supabaseClient
-        .from('profiles')
-        .select('*')
-        .eq('id', user.id)
-        .single();
-
-    if (error) {
-        return null; // Or handle error
-    }
-
-    return data;
-}
-
 // addLead adds a new lead to the waitlist table (for waiting list)
 export async function addWaitlist(email) {
     return supabase.from('waitlist').insert({email});
